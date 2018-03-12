dd9fca91b6b6bd77b957cab8b324ca9503760957
A K <andregk@gmail.com>
2018-03-12 13:57:31 -0300

    alteracoes na aplicacao

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def images(filename):
 def fonts(filename):
 	return static_file(filename, root='static/fonts')
 
-@route('/login') # @get('/login')
+@route('/') # @get('/login')
 def login():
 	return template('login')
 
@@ -45,7 +45,8 @@ def check_login(username, password):
 		return True
 	return False
 
-@route('/login', method='POST') # @post('/login')
+
+@route('/', method='POST') # @post('/login')
 def acao_login():
 	username = request.forms.get('username')
 	password = request.forms.get('password')
@@ -56,4 +57,4 @@ def error404(error):
 	return template('pagina404')
 
 if __name__ == '__main__':
-	run(host='localhost', port=8080, debug=True, reloader=True)
+	run(host='localhost', port=8080, debug=True, reloader=False)
